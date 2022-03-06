8090fc86e1008f18057d929eaafc7943e20ff194
Alex Yarem4uk <yarem4uk@gmail.com>
2022-03-06 12:43:22 +0200

    well done

---
+++ server.js
@@ -0,0 +1,37 @@
+const Express = require('express')
+// import Express from 'express'
+
+const app = new Express()
+
+const PORT = process.env.PORT || 8080
+
+
+app.listen(PORT, () => {
+  console.log(`server has been starteed...on port ${PORT}`)  
+})
+
+app.get('/', (req, res) =>{
+  res.end(`
+  <div>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+  </nav>
+    <h1>this is Home page</h1>
+  </div>`)    
+})
+
+app.get('/about', (req, res) => {
+  res.end(`
+  <div>
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+  </nav>
+    <h1>this is About page</h1>
+  </div>`)    
+})
