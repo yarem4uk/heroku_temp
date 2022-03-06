6de570ece9260df2397b187db554338f0a199804
Alex Yarem4uk <yarem4uk@gmail.com>
2022-03-06 12:53:54 +0200

    correct test page

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/test', (req, res) => {
   <div>
     <nav>
       <ul>
-        <li><a href="/">Test</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/test">Test</a></li>
       </ul>
