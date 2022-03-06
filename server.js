00fbec440d9451045c6f1f7162c5166e00f813fb
Alex Yarem4uk <yarem4uk@gmail.com>
2022-03-06 12:52:16 +0200

    add test page

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get('/', (req, res) =>{
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
+        <li><a href="/test">Test</a></li>
       </ul>
   </nav>
     <h1>this is Home page</h1>
@@ -30,6 +31,21 @@ app.get('/about', (req, res) => {
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
+        <li><a href="/test">Test</a></li>
+      </ul>
+  </nav>
+    <h1>this is About page</h1>
+  </div>`)    
+})
+
+app.get('/test', (req, res) => {
+  res.end(`
+  <div>
+    <nav>
+      <ul>
+        <li><a href="/">Test</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/test">Test</a></li>
       </ul>
   </nav>
     <h1>this is About page</h1>
